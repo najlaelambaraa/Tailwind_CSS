c85ecbe00ee54467c90809e4d0f5c0772bb77cfa
Najlae Lambaraa <Najlae.LAMBARAA@etu.uca.fr>
2024-05-12 16:34:16 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        grey: '#F4F4F4',
+        black: '#000',
+        'dark-medium': '#353535',
+        white: '#fff',
+        'light-black': '#232323',
+        purple: '#5D47E9',
+        mint: '#E4F6FF',
+        green: '#D3FF24',
+        blue: '#90e0ef',
+      },
+    
+    },
+  },
+  plugins: [],
+}
+
